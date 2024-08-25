3bcc7e507450822ed6887f65325b25ff438a042c
Bartosz Ciulkin <bartosz.ciulkin@gmail.com>
2024-08-25 19:33:50 +0200

    Introduce POST method

--- hello-world.go
+++ hello-world.go
@@ -9,12 +9,26 @@ import (
 
 func main() {
   
-  animal := model.NewAnimal("Salsa", 4)
-  animalJson, _ := json.Marshal(animal)
   // cat := model.Cat{model.NewAnimal("Wegorz", 2), true}
 
+  // GET /animal
   http.HandleFunc("/animal", func (w http.ResponseWriter, r *http.Request) {
+    var animal model.Animal = model.NewAnimal("Salsa", 4)
+    var animalJson, _ = json.Marshal(animal)
     fmt.Fprintf(w, string(animalJson))
+  })
+
+  // POST /test
+  http.HandleFunc("/test", func (rw http.ResponseWriter, req *http.Request) {
+    decoder := json.NewDecoder(req.Body)
+    var a model.Animal
+    err := decoder.Decode(&a)
+    if err != nil {
+        panic(err)
+    }
+
+    aJosn, _ := json.Marshal(a)
+    fmt.Fprintf(rw, string(aJosn))
   })
 
   http.ListenAndServe(":8080", nil)
