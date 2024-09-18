bf23b22611986b4194bdeca6496cccf1fcb37fcd
Bartosz Ciulkin <bartosz.ciulkin@gmail.com>
2024-09-18 16:45:10 +0200

    Fix local CLI command to save logs to external file

--- cli/cmd/local.go
+++ cli/cmd/local.go
@@ -3,6 +3,7 @@ package cmd
 import (
   "log"
   "os/exec"
+  "os"
   "github.com/spf13/cobra"
 )
 
@@ -16,7 +17,14 @@ var localCmd = &cobra.Command{
   Long:  `Runs go-by-example server locally.
   By default runs with MySql local default setup. More info in project's README`,
   Run: func(cmd *cobra.Command, args []string) {
-    command := exec.Command("go", "run", "main.go", "root", "new-password", "&>./app.log", "&")
+    command := exec.Command("go", "run", "main.go", "root", "new-password", "&")
+
+    outfile, ioerr := os.Create("./app.log")
+    if ioerr != nil {
+      log.Println("Can't open app.log file")
+    }
+    defer outfile.Close()
+    command.Stdout = outfile
 
     if err := command.Start(); err != nil {
       log.Println(err.Error())
