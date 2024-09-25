beb180bdc8329b029f2f4ca90d82c1a3f3316ceb
Bartosz Ciulkin <bartosz.ciulkin@gmail.com>
2024-09-25 10:47:16 +0200

    Tests runs, but fails

--- domain/animal-serivce_test.go
+++ domain/animal-serivce_test.go
@@ -2,8 +2,32 @@ package domain
 
 import (
   "testing"
+  "go-by-example/model"
+  "github.com/google/uuid"
 )
 
 func TestGetAnimals(t *testing.T) {
-  
+  repo := NewAnimalRepository()
+  service := NewAnimalService(repo)
+
+  got, err := service.GetAnimals()
+  if err != nil {
+    t.Errorf("service.GetAnimals error")
+  }
+  want := []model.Animal{
+    {
+      Name: "test",
+      Age: 1,
+      Id: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+    },
+    {
+      Name: "test2",
+      Age: 2,
+      Id: uuid.MustParse("00000000-0000-0000-0000-000000000002"),
+    },
+  }
+
+  if got[0] != want[0] {
+    t.Errorf("got %q wanted %q", got, want)
+  }
 }
