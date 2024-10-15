5d2db4c90118783c53395b18c42be4e09ef07fe2
Bartosz Ciulkin <bartosz.ciulkin@gmail.com>
2024-10-15 17:47:53 +0200

    LCS problem solution with 2d array

--- longest-common-subsequence/main.go
+++ longest-common-subsequence/main.go
@@ -15,7 +15,6 @@ func main() {
 	result := lcs(input, input2)
 	fmt.Println("Result: ", result)
 
-
 	input3 := "ABCDEFG"
 	input4 := "DEGJABCDAE"
 
@@ -27,8 +26,35 @@ func main() {
 	
 }
 
-func lcs(s1, s2 string) string {
-	return "ABC"
+func lcs(s1, s2 string) int {
+	m := len(s1)
+	n := len(s2)
+
+	// Create a 2D slice to store lengths of LCS for substrings
+	dp := make([][]int, m+1)
+
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	
+    	for i := 1; i <= m; i++ {
+        	for j := 1; j <= n; j++ {
+			//fmt.Println("s1: ", s1[i-1])
+			//fmt.Println("s2: ", s2[j-1])
+			if s1[i-1] == s2[j-1] {
+                		dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+                		dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	fmt.Println(dp)
+	return dp[m][n]
 }
 
-
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
