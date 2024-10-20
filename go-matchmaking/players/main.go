e5358185d22e6ef28535ce2294aa2f2a908b1f5b
Bartosz Ciulkin <bartosz.ciulkin@gmail.com>
2024-10-20 11:05:54 +0200

    WIP, spam with matchmaking service with players

--- go-matchmaking/players/main.go
+++ go-matchmaking/players/main.go
@@ -2,23 +2,31 @@ package main
 
 import (
   "net/http"
-  "log"
-  //"time"
+  "fmt"
+  "time"
 )
 
 func main() {
   // define worker pool (5)
   
   
-  p := NewPlayerParams()
-  log.Println(p)
-  http.PostForm("http://localhost:8080/player", p)
+  const numJobs = 10
+  jobs := make(chan int, numJobs)
+
+  for w := 1; w <= 4; w++ {
+    go worker(w, jobs)
+  }
+
+  for j := 1; j <= numJobs; j++ {
+    jobs <- j
+  }
+  close(jobs)
 }
 
-func worker(id int, jobs <-chan int, result chan<- int) {
-  for j := range jobs {
-    log.Println(j, id)
-  //  p := NewPlayerParams()
-    //resp, err := http.Post("http://localhost:8080/player", p)
-  }
+func worker(id int, jobs <-chan int) {
+    for j := range jobs {
+        time.Sleep(time.Second)
+        http.PostForm("http://localhost:8080/player", NewPlayerParams())
+        fmt.Println("Print, ", j)
+    }
 }
